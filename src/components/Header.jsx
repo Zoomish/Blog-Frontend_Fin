2bb543ab675785c68973d974113641de4f3895ca
Zoom <egor147536987@gmail.com>
2024-02-18 17:11:40 +0300

    Changed the header text to "Zoomish Blog" and added a gradient background to the text. (generated by blackbox.ai )

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <Navbar className='border-b-2'>
-      <Link to='/' className='flex items-center'>
-        <span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
+      <Link to='/' className='items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
           Zoomish
         </span>
         Blog
