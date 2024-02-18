1cd799c73a2fb8fea1965b4223cb09adfff1cd11
Zoom <egor147536987@gmail.com>
2024-02-18 15:36:25 +0300

    Update tailwind.config.js to use flowbite plugin (generated by blackbox.ai )

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,6 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
+    require('flowbite/plugin')
   ],
 }
