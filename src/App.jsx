5e3e381898583127ca7c2dc641b0cc8c0ca28d27
Zoom <egor147536987@gmail.com>
2024-02-18 15:12:07 +0300

    Added routes for About, Projects, SignIn, SingUp, and Dashboard pages. (generated by blackbox.ai )

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SingUp from './pages/SingUp'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/signin" element={<SignIn />} />
