1a4194204f3ac359c747a4d6ac1efd3a98ae8e34
Zoom <egor147536987@gmail.com>
2024-02-18 17:07:27 +0300

    Changed the logo and the name of the navbar brand from "Flowbite" to "Zoomish Blog". (generated by blackbox.ai )

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,12 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <Navbar className='border-b-2'>
-      <Navbar.Brand href="https://flowbite.com/">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite
+      <Link to='/' className='flex items-center'>
+        <span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
+          Zoomish
         </span>
-      </Navbar.Brand>
+        Blog
+      </Link>
     </Navbar>
   )
 }
