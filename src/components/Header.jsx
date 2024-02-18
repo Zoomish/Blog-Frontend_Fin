b81996dc03d61299812959cce09e1487d9b7a9f7
Zoom <egor147536987@gmail.com>
2024-02-18 20:28:49 +0300

    Update the navigation bar to highlight the current page. (generated by blackbox.ai )

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
 
 
 export default function Header() {
+  const path = useLocation().pathname
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className='items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -36,17 +37,17 @@ export default function Header() {
         <Navbar.Toggle/>
       </div>
       <Navbar.Collapse>
-          <Navbar.Link>
+          <Navbar.Link active={path === '/'}>
             <Link to='/'>
               Home
             </Link>
           </Navbar.Link>
-          <Navbar.Link>
+          <Navbar.Link active={path === '/about'}>
             <Link to='/about'>
               About
             </Link>
           </Navbar.Link>
-          <Navbar.Link>
+          <Navbar.Link active={path === '/projects'}>
             <Link to='/projects'>
               Projects
             </Link>
