80adaf37e841045d454036a5fb19f91ada4f8bce
Zoom <egor147536987@gmail.com>
2024-02-18 17:05:59 +0300

    Update header component to use Flowbite React Navbar component. (generated by blackbox.ai )

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
+import { Navbar } from 'flowbite-react';
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>header</div>
+    <Navbar className='border-b-2'>
+      <Navbar.Brand href="https://flowbite.com/">
+        <img
+          src="https://flowbite.com/docs/images/logo.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Flowbite
+        </span>
+      </Navbar.Brand>
+    </Navbar>
   )
 }
