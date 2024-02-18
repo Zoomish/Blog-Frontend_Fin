a29867ba38a0a516f0d886c1e3f864ba55536c8f
Zoom <egor147536987@gmail.com>
2024-02-18 15:17:24 +0300

    Changed the self-closing tag of the Home component from `<Home/>` to `<Home />`. (generated by blackbox.ai )

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/signin" element={<SignIn />} />
