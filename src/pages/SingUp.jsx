15dbe0e49137966bf68119339eb1d474a0963a50
Zoom <egor147536987@gmail.com>
2024-02-19 16:42:43 +0300

    Add a new div to the SingUp page. (generated by blackbox.ai )

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -1,8 +1,12 @@
 
 export default function SingUp() {
   return (
-    <div  className='min-h-screen mt-20'>
-      SingUp
+    <div className='min-h-screen mt-20'>
+      <div>
+        <div></div>
+        <div></div>
       </div>
+      SingUp
+    </div>
   )
 }
