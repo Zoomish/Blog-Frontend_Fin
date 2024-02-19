219f3a8484d19b670b8259838b6441fe967dd65f
Zoom <egor147536987@gmail.com>
2024-02-19 16:45:59 +0300

    Add Zoomish Blog header and description to the Signup page (generated by blackbox.ai )

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
 
 export default function SingUp() {
   return (
     <div className='min-h-screen mt-20'>
       <div>
-        <div className=""></div>
+        <div className="">
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+              Zoomish
+            </span>
+            Blog
+          </Link>
+          <p className="">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti repudiandae cupiditate blanditiis dignissimos incidunt laudantium recusandae fugiat unde illum, eos, velit architecto soluta ipsum enim hic ipsam accusantium vel quos!
+          </p>
+        </div>
         <div className=""></div>
       </div>
       SingUp
