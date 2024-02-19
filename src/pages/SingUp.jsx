10b96ec085c4bbc461fffaf42e5403876430ce87
Zoom <egor147536987@gmail.com>
2024-02-19 16:43:01 +0300

    Update the markup of the SingUp page. (generated by blackbox.ai )

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -3,8 +3,8 @@ export default function SingUp() {
   return (
     <div className='min-h-screen mt-20'>
       <div>
-        <div></div>
-        <div></div>
+        <div className=""></div>
+        <div className=""></div>
       </div>
       SingUp
     </div>
