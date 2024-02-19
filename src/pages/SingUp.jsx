072a0ab460decbed6135fe29f688033272a9effe
Zoom <egor147536987@gmail.com>
2024-02-19 17:16:56 +0300

    Update the left and right divs to use flexbox (generated by blackbox.ai )

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -6,7 +6,7 @@ export default function SingUp() {
     <div className='min-h-screen mt-20'>
       <div className="flex p-3 max-w-3xl justify-between mx-auto flex-col md:flex-row md:items-center">
         {/* left */}
-        <div className="">
+        <div className="flex-1">
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
               Zoomish
@@ -18,7 +18,7 @@ export default function SingUp() {
           </p>
         </div>
         {/* right */}
-        <div className="">
+        <div className="flex-1">
           <form className="flex flex-col gap-4">
             <div>
               <Label value='Your username' />
