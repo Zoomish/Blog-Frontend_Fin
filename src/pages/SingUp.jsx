635cfe6285b9b665875a06b9c8c6b73811ab5f4f
Zoom <egor147536987@gmail.com>
2024-02-19 17:13:48 +0300

    Changed the spelling of "SingUp" to "SignUp" in the file path. (generated by blackbox.ai )

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import {Label, TextInput } from 'flowbite-react';
+import { Label, TextInput } from 'flowbite-react';
 
 export default function SingUp() {
   return (
