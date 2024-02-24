3e2c3552f3825b3a6b22f0bda997663c16f48811
Zoom <egor147536987@gmail.com>
2024-02-24 19:00:37 +0300

    Update link to About page to use noreferrer and noopener attributes (generated by blackbox.ai )

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ export default function FooterComponent() {
                     <div className='gird grid-cols-2 gap-3 sm:mt-4 sm:gird-cols-4 sm:gap-6'>
                         <Footer.Title title="About" />
                         <Footer.LinkGroup col>
-                            <Footer.Link href="/about" target='_blank'>About</Footer.Link>
+                            <Footer.Link href="/about" target='_blank' rel='noreferrer noopener'>About</Footer.Link>
                         </Footer.LinkGroup>
                     </div>
                 </div>
