7347bdaf9ae2fbb56bd865f99033e055ab85c049
Zoom <egor147536987@gmail.com>
2024-02-24 19:01:48 +0300

    Add "About" link to the footer. (generated by blackbox.ai )

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,10 +15,13 @@ export default function FooterComponent() {
                         </Link>
                     </div>
                     <div className='gird grid-cols-2 gap-3 sm:mt-4 sm:gird-cols-4 sm:gap-6'>
-                        <Footer.Title title="About" />
-                        <Footer.LinkGroup col>
-                            <Footer.Link href="/about" target='_blank' rel='noreferrer noopener'>About</Footer.Link>
-                        </Footer.LinkGroup>
+                        <div>
+                            <Footer.Title title="About" />
+                            <Footer.LinkGroup col>
+                                <Footer.Link href="/about" target='_blank' rel='noreferrer noopener'>About</Footer.Link>
+                            </Footer.LinkGroup>
+                        </div>
+
                     </div>
                 </div>
             </div>
