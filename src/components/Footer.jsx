aebf95a927136c726023b766a191f88ed8e4f8fe
Zoom <egor147536987@gmail.com>
2024-02-24 17:45:55 +0300

    Update footer to include a link to the blog. (generated by blackbox.ai )

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import { Footer } from 'flowbite-react'
+import { Link } from 'react-router-dom'
 
 export default function FooterComponent() {
     return (
-        <Footer container className="border border-t-8 border-teal-500">Footer</Footer>
+        <Footer container className="border border-t-8 border-teal-500">
+            <div>
+                <div>
+                    <div>
+                        <Link to='/' className='items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+                            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                                Zoomish
+                            </span>
+                            Blog
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </Footer>
     )
 }
