19176dee4ffd968e0d0357881bf1f6afc751fcb3
Zoom <egor147536987@gmail.com>
2024-03-02 16:14:15 +0300

    Middleware configuration to allow non-serializable state in Redux store. (generated by blackbox.ai )

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,5 +17,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(
+        { serializableCheck: false }
+    )
 })
