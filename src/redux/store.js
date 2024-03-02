fb10f37148c60a4848277f1b54cd846f2ca2a1fe
Zoom <egor147536987@gmail.com>
2024-03-02 16:09:08 +0300

    Add CombineReducers to the import statement. (generated by blackbox.ai )

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, CombineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+
 
 export const store = configureStore({
     reducer: {
