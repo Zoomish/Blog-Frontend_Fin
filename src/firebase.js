07870ecc7ef76d61b3d341cc0970c585ee3b426d
Zoom <egor147536987@gmail.com>
2024-03-02 17:27:14 +0300

    Update firebase.js to remove the console.log statement. (generated by blackbox.ai )

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-console.log(import.meta.env.VITE_FIREBASE_API_KEY);
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
