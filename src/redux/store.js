5e4298a10c3099e32afa1b00187783a72842fdf2
Zoom <egor147536987@gmail.com>
2024-03-02 16:09:30 +0300

    Combine multiple reducers into a root reducer. (generated by blackbox.ai )

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore, CombineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 
+const rootReducer = CombineReducers({
+    user: userReducer
+})
 
 export const store = configureStore({
     reducer: {
-        user: userReducer
+        rootReducer
     },
 })
