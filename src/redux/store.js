16d29e7950f14fe284df9d9026fe0b40f1e59f9c
Zoom <egor147536987@gmail.com>
2024-03-02 16:27:24 +0300

    Combine Redux reducers using `combineReducers` instead of `CombineReducers`. (generated by blackbox.ai )

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import persistStore from 'redux-persist/es/persistStore'
-import { CombineReducers } from 'redux'
 
-const rootReducer = CombineReducers({
+const rootReducer = combineReducers({
     user: userReducer
 })
 
