d7359e982796f1e55e82889d97d426641d73b2bd
Zoom <egor147536987@gmail.com>
2024-03-02 17:44:19 +0300

    Add useNavigate import and update variable name (generated by blackbox.ai )

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -4,11 +4,12 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from "react-redux";
 import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
     const auth = getAuth(app);
     const dispatch = useDispatch()
-    const navigete = useNavigate()
+    const navigate = useNavigate()
 
     const handleGoogleClick = async () => {
         const provider = new GoogleAuthProvider();
