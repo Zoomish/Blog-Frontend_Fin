840a87da197796f7df155165cee5f596b46f523b
Zoom <egor147536987@gmail.com>
2024-03-02 16:10:24 +0300

    Add redux-persist to the project. (generated by blackbox.ai )

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore, CombineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import { persistReducer } from 'redux-persist'
 
 const rootReducer = CombineReducers({
     user: userReducer
+})
+
+const persistedReducer = persistReducer({
+    key: 'root',
 })
 
 export const store = configureStore({
