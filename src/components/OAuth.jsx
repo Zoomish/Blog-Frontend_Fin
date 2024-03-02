87052441138e5dbfbffd0e61912d8f2a2036ddb5
Zoom <egor147536987@gmail.com>
2024-03-02 17:07:40 +0300

    Add a Google OAuth button. (generated by blackbox.ai )

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -2,8 +2,11 @@ import { Button } from 'flowbite-react';
 import { AiFillGoogleCircle } from 'react-icons/ai';
 
 export default function OAuth() {
+    const handleGoogleClick = () => {
+        
+    }
     return (
-        <Button type='button' gradientDuoTone='pinkToOrange' outline>
+        <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
             <AiFillGoogleCircle className='w-6 h-6 mr-2'/> Continue with Google
         </Button>
     )
