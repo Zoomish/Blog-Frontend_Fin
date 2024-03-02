afdc25c9f7911f6db60348cdb4febe0dc898de28
Zoom <egor147536987@gmail.com>
2024-03-02 16:16:11 +0300

    Added redux-persist to the project. (generated by blackbox.ai )

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
