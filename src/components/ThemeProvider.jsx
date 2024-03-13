e68f4b68ec9d54cd1723f48fb69b23cee6840b0e
Zoom <egor147536987@gmail.com>
2024-03-13 21:40:15 +0300

    Changed the return statement from a div with the text "ThemeProvider" to a div with the variable "theme".

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -1,7 +1,7 @@
 
 export default function ThemeProvider() {
     const theme = useSelector(state => state.theme)
-  return (
-    <div>ThemeProvider</div>
-  )
+    return (
+        <div>ThemeProvider</div>
+    )
 }
