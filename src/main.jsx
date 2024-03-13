fb738f12a6dce6c1b2b999a4aa6d452d63dac421
Zoom <egor147536987@gmail.com>
2024-03-13 21:47:52 +0300

    Added a ThemeProvider component to wrap the App component.

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate loading={null} persistor={persistor}>
     <Provider store={store}>
       <React.StrictMode>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </React.StrictMode>,
     </Provider>
   </PersistGate>
