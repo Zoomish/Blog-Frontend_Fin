9bd121cc024bd1ee83338a7c5128c55de4a4e653
Zoom <egor147536987@gmail.com>
2024-03-13 21:41:47 +0300

    Wrap children in ThemeProvider component

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -1,10 +1,10 @@
 import { useSelector } from 'react-redux'
 
-export default function ThemeProvider() {
+export default function ThemeProvider({ children }) {
     const theme = useSelector(state => state.theme)
     return (
         <div className={theme}>
-
+            {children}
         </div>
     )
 }
