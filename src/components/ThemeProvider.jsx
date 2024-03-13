5fe04b3d497ac130c6e2e0370f347a8e1063bfbe
Zoom <egor147536987@gmail.com>
2024-03-13 21:47:14 +0300

    Changed the background color of the app from black to white in light mode and from white to a dark blue in dark mode. Also changed the text color to match the new background colors.

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ export default function ThemeProvider({ children }) {
     const theme = useSelector(state => state.theme)
     return (
         <div className={theme}>
-            <div className=''>
+            <div className='bg-white dark:bg-[rgb(17, 24, 39)] text-gary-700 dark:text-gray-200'>
                 {children}
             </div>
         </div>
