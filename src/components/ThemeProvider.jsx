15f84ece48459079e890c6f675fd1f05dbd32fac
Zoom <egor147536987@gmail.com>
2024-03-13 21:41:18 +0300

    Changed the component to return a div with a class name set to the current theme.

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux'
 export default function ThemeProvider() {
     const theme = useSelector(state => state.theme)
     return (
-        <div>ThemeProvider</div>
+        <div className={theme}>
+
+        </div>
     )
 }
