aa5670a23548a57f27918e6ca05364989386aa1b
Zoom <egor147536987@gmail.com>
2024-03-13 21:58:30 +0300

    Change `theme` to `theme.theme` in `useSelector`

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 export default function ThemeProvider({ children }) {
-    const theme = useSelector(state => state.theme);
+    const theme = useSelector(state => state.theme.theme);
 
     return (
         <div className={theme}>
