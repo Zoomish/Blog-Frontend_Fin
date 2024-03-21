ff3697681e858f7b3cac0afe51b75e656f9f5f0b
Zoom <egor147536987@gmail.com>
2024-03-21 20:16:38 +0300

    "Add Sidebar.Items and Sidebar.ItemGroup components to DashSidebar"
    This commit adds the `Sidebar.Items`, `Sidebar.ItemGroup`, and `Sidebar.Item` components to the `DashSidebar` component. The `Sidebar.Item` components have `href` and `icon` props set to specify the links and icons for the sidebar items.

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -3,7 +3,12 @@ import { Sidebar } from 'flowbite-react'
 export default function DashSidebar() {
     return (
         <Sidebar>
-
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Sidebar.Item href='/dashboard' icon={true}>Dashboard</Sidebar.Item>
+                    <Sidebar.Item href='/dashboard?tab=profile' icon={true}>Profile</Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
         </Sidebar>
     )
 }
