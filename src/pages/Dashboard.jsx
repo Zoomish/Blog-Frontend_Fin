92dc0f9dbd1b9bdbe58c55cd47a682610895355b
Zoom <egor147536987@gmail.com>
2024-03-21 19:48:40 +0300

    "Add useEffect to set tab state from URL query parameter"
    This commit adds a useEffect hook to the Dashboard component that sets the tab state based on the 'tab' query parameter in the location object. The added code also logs the 'tab' query parameter to the console.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export default function Dashboard() {
   const location = useLocation()
+  const [tab, setTab] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    console.log(tabFromUrl);
+  }, [location])
   return (
     <div>Dashboard</div>
   )
