22e999694c69d1388148dcd3225e801a415c82c8
Zoom <egor147536987@gmail.com>
2024-03-21 19:37:22 +0300

    "Add PrivateRoute component to wrap protected routes"
    This commit adds a new component called PrivateRouter which is used to wrap protected routes in the application. In this case, the dashboard route is wrapped with PrivateRouter to ensure that only authenticated users can access it.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import SingUp from './pages/SingUp'
 import Dashboard from './pages/Dashboard'
 import FooterComponent from './components/Footer'
 import Header from './components/Header'
+import PrivateRouter from './components/PrivateRouter'
 
 
 export default function App() {
@@ -19,7 +20,9 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SingUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRouter />} >
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
       <FooterComponent />
     </BrowserRouter>
