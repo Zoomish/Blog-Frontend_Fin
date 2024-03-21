a96540a48357ed6ed117f790023142e3dabf322d
Zoom <egor147536987@gmail.com>
2024-03-21 20:44:43 +0300

    Update Dashboard.jsx: adjust div width for sidebar on medium screens
    * Changed the width of the first div to 'md:w-56' to adjust the sidebar width on medium-sized screens.
    * No functional changes, only cosmetic adjustment for improved layout.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,7 +15,7 @@ export default function Dashboard() {
   }, [location.search])
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
-      <div>
+      <div className='md:w-56'>
         {/* 'Sidebar' */}
         <DashSidebar />
       </div>
