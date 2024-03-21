f10b60eb6d78c15ee1aff8fbb62a99f9462e7bf3
Zoom <egor147536987@gmail.com>
2024-03-21 20:11:06 +0300

    "Add functionality to set tab from URL query parameter"
    This commit updates the Dashboard component to read a 'tab' query parameter from the URL and set the current tab accordingly. Previously, the 'tabFromUrl' variable was simply being logged to the console.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,9 @@ export default function Dashboard() {
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search)
     const tabFromUrl = urlParams.get('tab')
-    console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
   }, [location.search])
   return (
     <div>
