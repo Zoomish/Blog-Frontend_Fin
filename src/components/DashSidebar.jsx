307d9f9ea1b4b5ddf5f98272420558358a2046c6
Zoom <egor147536987@gmail.com>
2024-03-21 20:35:48 +0300

    "Add state for current tab and use location search parameter to set it"
    This commit updates the DashSidebar component to include state for the current tab and uses the search parameter of the location object from react-router-dom to set the tab when the component is rendered. This allows for the tab to be set based on the URL, so that when a user navigates to a specific tab, that tab will be selected when the page loads.

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,7 +1,18 @@
 import { Sidebar } from 'flowbite-react'
 import { HiArrowSmRight, HiUser } from 'react-icons/hi'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function DashSidebar() {
+    const location = useLocation()
+    const [tab, setTab] = useState('')
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+        const tabFromUrl = urlParams.get('tab')
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+    }, [location.search])
     return (
         <Sidebar>
             <Sidebar.Items>
