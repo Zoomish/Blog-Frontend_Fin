7b70f595c51c737e287fab1973a3ae6e0ce7debf
Zoom <egor147536987@gmail.com>
2024-03-21 20:27:37 +0300

    Update DashSidebar component with correct icons
    The diff updates the `DashSidebar` component to use the correct icons for the `Sidebar.Item` components. The `HiUSer` icon is used for the 'Profile' item and the `HiArrowSmRight` icon is used for the 'Sign Out' item.

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,12 +1,12 @@
 import { Sidebar } from 'flowbite-react'
-import { HiArrowSmRight, HiUSer } from 'react-icons/hi'
+import { HiArrowSmRight, HiUser } from 'react-icons/hi'
 
 export default function DashSidebar() {
     return (
         <Sidebar>
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Sidebar.Item active icon={HiUSer} label={'User'} labelColor='dark'>Profile</Sidebar.Item>
+                    <Sidebar.Item active icon={HiUser} label={'User'} labelColor='dark'>Profile</Sidebar.Item>
                     <Sidebar.Item active icon={HiArrowSmRight} label={'User'} labelColor='dark'>Sign Out</Sidebar.Item>
                 </Sidebar.ItemGroup>
             </Sidebar.Items>
