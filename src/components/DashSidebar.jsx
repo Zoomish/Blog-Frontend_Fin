06116c83118ea88d7ebc8b180761d157eafa5299
Zoom <egor147536987@gmail.com>
2024-03-21 20:17:38 +0300

    "Replace 'Dashboard' sidebar item icon with HiUser icon and set as active"
    This commit updates the 'Dashboard' sidebar item in the DashSidebar component by replacing the previous icon with the HiUser icon from the 'react-icons/hi' library and setting it as active.

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,11 +1,12 @@
 import { Sidebar } from 'flowbite-react'
+import { HiUSer } from 'react-icons/hi'
 
 export default function DashSidebar() {
     return (
         <Sidebar>
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Sidebar.Item href='/dashboard' icon={true}>Dashboard</Sidebar.Item>
+                    <Sidebar.Item active icon={HiUSer}>Dashboard</Sidebar.Item>
                     <Sidebar.Item href='/dashboard?tab=profile' icon={true}>Profile</Sidebar.Item>
                 </Sidebar.ItemGroup>
             </Sidebar.Items>
