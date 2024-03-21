cd8e8a6416b7d6155526de768478ad0144686b0e
Zoom <egor147536987@gmail.com>
2024-03-21 19:53:31 +0300

    "Add condition to render DashProfile based on tab value"
    This commit message describes the change in the code diff where the rendering of the `DashProfile` component is now conditionally based on the value of the `tab` variable.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
         <DashSidebar />
       </div>
       {/* Profile and etc */}
-      <DashProfile />
+      { tab === 'profile' && <DashProfile />}
     </div>
   )
 }
