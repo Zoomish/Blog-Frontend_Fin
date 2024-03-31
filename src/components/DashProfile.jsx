04e6d5ba26256ed0ee59dd53a43cde628c53a8af
Zoom <egor147536987@gmail.com>
2024-03-31 17:21:36 +0300

    Update DashProfile.jsx: change user profile picture div to include overflow-hidden and rounded-full
    The changes in this diff include updating the user profile picture div to include the classes "overflow-hidden" and "rounded-full". This is likely to improve the appearance of the profile picture by ensuring that it is fully contained within the div and has rounded corners.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -6,7 +6,7 @@ export default function DashProfile() {
         <div>
             <h1>Profule</h1>
             <form className="flex flex-col">
-                <div className="self-center w-32 h-32 cursor-pointer shadow-md">
+                <div className="self-center w-32 h-32 cursor-pointer shadow-md overflow-hidden rounded-full">
                     <img src={currentUser.profilePicture} alt="User" className="rounded-full object-cover w-full h-full border-8 border-[lightgray]" />
                 </div>
             </form>
