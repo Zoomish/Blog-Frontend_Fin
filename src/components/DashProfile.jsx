679a666c4ed002649eb3f841bc920160a0044534
Zoom <egor147536987@gmail.com>
2024-03-31 13:07:53 +0300

    "Import useSelector hook from react-redux and use it to get currentUser from state in DashProfile component"

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function DashProfile() {
+    const { currentUser } = useSelector(state => state.user)
     return (
         <div>DashProfile</div>
     )
