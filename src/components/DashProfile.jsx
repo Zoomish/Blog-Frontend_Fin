0b13a362db3246aa6c1a2423858480c2cd177b71
Zoom <egor147536987@gmail.com>
2024-03-31 13:09:16 +0300

    Update DashProfile component to display profile picture
    The changes include:
    - Adding an image tag to display the current user's profile picture
    - Wrapping the heading and image in a form element
    - Wrapping the entire component in a div element
    
    Note: The currentUser object is assumed to contain a 'profilePicture' property that contains the URL of the user's profile picture.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -3,6 +3,11 @@ import { useSelector } from 'react-redux'
 export default function DashProfile() {
     const { currentUser } = useSelector(state => state.user)
     return (
-        <div>DashProfile</div>
+        <div>
+            <h1>Profule</h1>
+            <form>
+                <img src={currentUser.profilePicture} alt="" />
+            </form>
+        </div>
     )
 }
