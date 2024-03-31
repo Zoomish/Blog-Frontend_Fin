894d288d5a8d46a0caba60e2b399c12927cba869
Zoom <egor147536987@gmail.com>
2024-03-31 17:22:34 +0300

    Update DashProfile component to add margin and padding
    This commit updates the `DashProfile` component by adding `mx-auto` to the `div` element's class to center it horizontally, and adding `p-3` to add padding to the element. The `w-full` class is also added to ensure that the element takes up the full width of its container.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 export default function DashProfile() {
     const { currentUser } = useSelector(state => state.user)
     return (
-        <div className="max-w-lg ">
+        <div className="max-w-lg mx-auto p-3 w-full">
             <h1>Profule</h1>
             <form className="flex flex-col">
                 <div className="self-center w-32 h-32 cursor-pointer shadow-md overflow-hidden rounded-full">
