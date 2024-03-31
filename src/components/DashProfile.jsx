30c106bc1a698123ec43c812e21a481e9951ecde
Zoom <egor147536987@gmail.com>
2024-03-31 17:23:39 +0300

    Update DashProfile.jsx: add className to h1 tag for styling
    This commit updates the DashProfile.jsx component by adding a className attribute to the h1 tag, allowing for custom styling of the "Profile" heading.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -4,7 +4,7 @@ export default function DashProfile() {
     const { currentUser } = useSelector(state => state.user)
     return (
         <div className="max-w-lg mx-auto p-3 w-full">
-            <h1>Profile</h1>
+            <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
             <form className="flex flex-col">
                 <div className="self-center w-32 h-32 cursor-pointer shadow-md overflow-hidden rounded-full">
                     <img src={currentUser.profilePicture} alt="User" className="rounded-full object-cover w-full h-full border-8 border-[lightgray]" />
