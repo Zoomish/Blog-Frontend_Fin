d2a3ffb57467f33e2e1e36b9bbffa650aa15d8f8
Zoom <egor147536987@gmail.com>
2024-03-31 18:05:03 +0300

    Add 'as' prop to Sidebar.Item component for Profile link in DashSidebar
    This diff adds the 'as' prop to the Sidebar.Item component for the Profile link in DashSidebar, with a value of 'div'. This allows the component to maintain its active state when the link is clicked.

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -18,7 +18,7 @@ export default function DashSidebar() {
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
                     <Link to='/dashboard?tab=profile'>
-                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'>Profile</Sidebar.Item>
+                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark' as='div'>Profile</Sidebar.Item>
                     </Link>
                     <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>Sign Out</Sidebar.Item>
                 </Sidebar.ItemGroup>
