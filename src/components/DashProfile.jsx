1ae2da2907a2c5144e52fc18dae5c7eea1c85970
Zoom <egor147536987@gmail.com>
2024-03-31 18:20:27 +0300

    "Update DashProfile.jsx with comments for firebase storage rules; remove unused uploadImage function"
    This commit updates the file DashProfile.jsx by adding comments related to firebase storage rules and removing the unused uploadImage function. The new firebase storage rules are added as a comment block, and the uploadImage function, which was not being used in the code, has been removed.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -22,9 +22,16 @@ export default function DashProfile() {
     }, [imageFile])
 
     const uploadImage = async () => {
-        const formData = new FormData()
-        formData.append('file', imageFile)
-        formData.append('upload_preset', 'zoomish')
+        // service firebase.storage {
+        //     match /b/{bucket}/o {
+        //       match /{allPaths=**} {
+        //         allow read;
+        //         allow write: if
+        //         request.resource.size < 2 * 1024 * 1024 &&
+        //         request.resource.contentType.matches('image/.*')
+        //       }
+        //     }
+        //   }
     }
 
     return (
