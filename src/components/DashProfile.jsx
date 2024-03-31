d27674a9b2102efbc8edfb5aedc2420172fd6273
Zoom <egor147536987@gmail.com>
2024-03-31 13:14:31 +0300

    Update DashProfile form and image container styles
    The changes in this diff include:
    
    * Wrapping the form in a `flex flex-col gap-4` class to vertically align the form elements.
    * Changing the `flex justify-center w-32 h-32` class on the image container to `self-center w-32 h-32` to center the image horizontally and vertically within the container.
    
    These changes improve the layout and styling of the DashProfile component.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -5,8 +5,8 @@ export default function DashProfile() {
     return (
         <div>
             <h1>Profule</h1>
-            <form>
-                <div className="flex justify-center w-32 h-32 ">
+            <form className="flex flex-col">
+                <div className="self-center w-32 h-32 ">
                     <img src={currentUser.profilePicture} alt="User" className="rounded-full object-cover w-full h-full border-8 border-[lightgray]" />
                 </div>
             </form>
