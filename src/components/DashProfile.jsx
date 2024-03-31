62ca29904520bef24a6228122f34144cb2121010
Zoom <egor147536987@gmail.com>
2024-03-31 17:24:37 +0300

    "Add TextInput component from flowbite-react to DashProfile"
    This commit adds the TextInput component from the flowbite-react library to the DashProfile component. The TextInput component is not used in the current implementation and does not have any props passed to it.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import { TextInput } from 'flowbite-react'
 
 export default function DashProfile() {
     const { currentUser } = useSelector(state => state.user)
@@ -9,6 +10,7 @@ export default function DashProfile() {
                 <div className="self-center w-32 h-32 cursor-pointer shadow-md overflow-hidden rounded-full">
                     <img src={currentUser.profilePicture} alt="User" className="rounded-full object-cover w-full h-full border-8 border-[lightgray]" />
                 </div>
+                <TextInput/>
             </form>
         </div>
     )
