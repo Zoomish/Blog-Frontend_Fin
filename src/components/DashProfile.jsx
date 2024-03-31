b2f061d566b9b953dcdf9481000cfd4dff481139
Zoom <egor147536987@gmail.com>
2024-03-31 17:58:59 +0300

    "Update DashProfile.jsx: add state for image file and handleImageChange function"
    This commit updates the DashProfile.jsx component by adding a state variable for storing the selected image file and a handleImageChange function to update the state when a file is selected. The input element for file selection is also updated with the onChange handler.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -5,11 +5,16 @@ import { useState } from 'react'
 export default function DashProfile() {
     const { currentUser } = useSelector(state => state.user)
     const [imageFile, setImageFile] = useState(null)
+
+    const handleImageChange = (e) => {
+        setImageFile(e.target.files[0])
+    }
+
     return (
         <div className="max-w-lg mx-auto p-3 w-full">
             <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
             <form className="flex flex-col gap-4">
-                <input type="file" accept='image/*' onChange={handleImageChange}/>
+                <input type="file" accept='image/*' onChange={handleImageChange} />
                 <div className="self-center w-32 h-32 cursor-pointer shadow-md overflow-hidden rounded-full">
                     <img src={currentUser.profilePicture} alt="User" className="rounded-full object-cover w-full h-full border-8 border-[lightgray]" />
                 </div>
