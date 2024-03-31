9e5f9995600c6104b6c4533202318269e82175b9
Zoom <egor147536987@gmail.com>
2024-03-31 18:08:02 +0300

    Add ref to file input in DashProfile component
    This commit adds a ref to the file input element in the DashProfile component, which can be used to access the DOM node and programmatically focus or interact with the element.

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -20,7 +20,7 @@ export default function DashProfile() {
         <div className="max-w-lg mx-auto p-3 w-full">
             <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
             <form className="flex flex-col gap-4">
-                <input type="file" accept='image/*' onChange={handleImageChange} />
+                <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} />
                 <div className="self-center w-32 h-32 cursor-pointer shadow-md overflow-hidden rounded-full">
                     <img src={imageFileUrl || currentUser.profilePicture} alt="User" className="rounded-full object-cover w-full h-full border-8 border-[lightgray]" />
                 </div>
